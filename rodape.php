4dd5b27acad67b4d8b55f9d947083ed284988f59
Mateus Seiboth <mateus.seiboth@estudante.ifms.edu.br>
2022-12-06 17:30:03 -0400

    Atualizada mensagem da versão

--- rodape.php
+++ rodape.php
@@ -1,11 +1,11 @@
 
-<footer class="bg-light me-auto text-dark fixed-bottom ">
+<footer class="bg-light me-auto text-dark fixed-bottom">
     <div>
       
         <span class="pull-left">© 2022 RYMTech</span>
 	
-	    <span class="pull-right me-auto"><i class="fa fa-server fa-fw"></i>Server version: 0.1 Alpha </span>
-	    <span class="pull-right me-auto"><i class="fa fa-database fa-fw"></i>Database version: 0.1 Alpha</span>
+	    <span class="pull-right me-auto" style="margin-left: 10px;"><i class="fa fa-server fa-fw"></i>Server version: v0.1 release</span>
+	    <span class="pull-right me-auto"  ><i class="fa fa-database fa-fw"></i>Database version: v0.2 release</span>
 	
     </div>
 </footer>
